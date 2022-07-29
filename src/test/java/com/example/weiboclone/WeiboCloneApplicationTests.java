f76ed7322f1f2c3b0dcbfd36ada2918148f741a0
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2022-07-29 18:13:19 +0900

    Initial commit

---
+++ src/test/java/com/example/weiboclone/WeiboCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weiboclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeiboCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
