a589bef90def7c6defdbcd3fc9f1c0ae7195ccb6
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2022-07-29 20:54:45 +0900

    [model entity] 추가

---
+++ src/main/java/com/example/weiboclone/model/Users.java
@@ -0,0 +1,34 @@
+package com.example.weiboclone.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@DynamicUpdate
+@Entity
+public class Users{
+    @Id  //ID 할당 방법 1.직접 넣는 방식 (Setter, 생성자) 2.(JPA나)DB에게 할당 책임을 전가. (@GenerateValue)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // mysql 은 identity. auto는 안 맞을 경우도 있어.
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String userprofileimage;
+
+
+    public Users(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
