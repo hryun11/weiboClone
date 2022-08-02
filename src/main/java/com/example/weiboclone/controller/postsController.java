c33aca9e553a9268d9a4f040d11de53316e3aab6
LAPTOP-4RG8174M\User <wjdgns5488@naver.com>
2022-08-02 21:03:24 +0900

    [오류 수정] - PostServie 중복 제거

--- src/main/java/com/example/weiboclone/controller/postsController.java
+++ src/main/java/com/example/weiboclone/controller/postsController.java
@@ -1,14 +1,12 @@
 package com.example.weiboclone.controller;
 
 import com.example.weiboclone.dto.PostsResponseDto;
-import com.example.weiboclone.service.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Map;
 
 
 @RequiredArgsConstructor
